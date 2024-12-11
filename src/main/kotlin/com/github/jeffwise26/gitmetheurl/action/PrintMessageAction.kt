107ac1315f3582e26af7f0888e5577246a37e7b8
jeffwise26 <jeffwise26@gmail.com>
2024-12-11 10:41:41 -0500

    feat/notification-instead-of-message-popup

--- src/main/kotlin/com/github/jeffwise26/gitmetheurl/action/PrintMessageAction.kt
+++ src/main/kotlin/com/github/jeffwise26/gitmetheurl/action/PrintMessageAction.kt
@@ -1,12 +1,21 @@
 package com.github.jeffwise26.gitmetheurl.action
 
 import com.github.jeffwise26.gitmetheurl.MyBundle
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.ui.Messages
+import javax.swing.Timer
 
 class PrintMessageAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        Messages.showMessageDialog(e.project, MyBundle.message("message") , MyBundle.message("messageTitle"), Messages.getInformationIcon())
+        val notification = Notification(
+            MyBundle.message("notificationGroup"),
+            MyBundle.message("notificationMessage"),
+            NotificationType.INFORMATION
+        )
+        Notifications.Bus.notify(notification)
+        Timer(1000) { notification.expire() }.start()
     }
 }
