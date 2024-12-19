203731a7db7628c0bb45cf0dea6aeb4175ab6fe0
jeffwise26 <jeffwise26@gmail.com>
2024-12-19 13:53:27 -0500

    working dependencies and dump repository information to clipboard

--- src/main/kotlin/com/github/jeffwise26/gitmetheurl/action/GitMeTheUrlAction.kt
+++ src/main/kotlin/com/github/jeffwise26/gitmetheurl/action/GitMeTheUrlAction.kt
@@ -1,18 +1,32 @@
 package com.github.jeffwise26.gitmetheurl.action
 
 import com.github.jeffwise26.gitmetheurl.MyBundle
+import com.github.weisj.jsvg.e
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.*
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import git4idea.GitUtil
 import java.awt.datatransfer.StringSelection
 import javax.swing.Timer
 
 class GitMeTheUrlAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        val gitUrl = "todo"
+        val file = runReadAction {
+            e.getData(com.intellij.openapi.actionSystem.CommonDataKeys.VIRTUAL_FILE)
+        }
+        val project = runReadAction {
+            e.project
+        }!!
+        val gitUrl = gitGitHubUrl(project, file!!)
         CopyPasteManager.getInstance().setContents(StringSelection(gitUrl));
         val notification = Notification(
             MyBundle.message("notificationGroup"),
@@ -21,5 +35,13 @@ class GitMeTheUrlAction : AnAction() {
         )
         Notifications.Bus.notify(notification)
         Timer(1000) { notification.expire() }.start()
+    }
+
+    fun gitGitHubUrl(project: Project, file: VirtualFile): String? {
+        val editor: Editor? = FileEditorManager.getInstance(project).selectedTextEditor
+
+        val currentFile = FileDocumentManager.getInstance().getFile(editor!!.getDocument())
+        val repository = GitUtil.getRepositoryManager(project).getRepositoryForFileQuick(currentFile)
+        return repository!!.remotes.joinToString(",")
     }
 }
