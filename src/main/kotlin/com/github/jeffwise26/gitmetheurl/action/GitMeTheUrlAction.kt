c67fbd164cfa9f6e68ec44c96aa79e84a20acc27
jeffwise26 <jeffwise26@gmail.com>
2024-12-30 14:19:45 -0500

    chore/polish-and-prep-for-release

--- src/main/kotlin/com/github/jeffwise26/gitmetheurl/action/GitMeTheUrlAction.kt
+++ src/main/kotlin/com/github/jeffwise26/gitmetheurl/action/GitMeTheUrlAction.kt
@@ -6,6 +6,7 @@ import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys.VIRTUAL_FILE
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
@@ -19,9 +20,9 @@ import javax.swing.Timer
 class GitMeTheUrlAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val gitUrl = gitGitHubUrl(
+        val gitUrl = parseGitHubUrl(
             e.project,
-            e.getData(com.intellij.openapi.actionSystem.CommonDataKeys.VIRTUAL_FILE)
+            e.getData(VIRTUAL_FILE)
         )
         val notification: Notification = buildNotification(gitUrl)
         when (gitUrl) {
@@ -44,22 +45,23 @@ class GitMeTheUrlAction : AnAction() {
         }
     )
 
-    private fun gitGitHubUrl(project: Project?, file: VirtualFile?): GitHubUrl {
+    private fun parseGitHubUrl(project: Project?, file: VirtualFile?): GitHubUrl {
         file ?: return GitHubUrlFail(MyBundle.message("notificationErrorFile"))
         project ?: return GitHubUrlFail(MyBundle.message("notificationErrorProject"))
 
-        val editor: Editor? = FileEditorManager.getInstance(project).selectedTextEditor
-        editor ?: return GitHubUrlFail(MyBundle.message("notificationErrorEditor"))
+        val editor = FileEditorManager.getInstance(project).selectedTextEditor
+            ?: return GitHubUrlFail(MyBundle.message("notificationErrorEditor"))
+
         val currentFile = FileDocumentManager.getInstance().getFile(editor.document)
         val repository = GitUtil.getRepositoryManager(project).getRepositoryForFileQuick(currentFile)
 
         val remoteUrl = repository?.remotes?.firstOrNull()?.firstUrl
-        remoteUrl ?: return GitHubUrlFail(MyBundle.message("notificationErrorRemote"))
+            ?: return GitHubUrlFail(MyBundle.message("notificationErrorRemote"))
 
         val lineNumber = editor.caretModel.logicalPosition.line
 
         val basePath = project.basePath
-        basePath ?: return GitHubUrlFail(MyBundle.message("notificationErrorProjectBasePath"))
+            ?: return GitHubUrlFail(MyBundle.message("notificationErrorProjectBasePath"))
 
         val filePath = file.path.substring(basePath.length)
 
